dfb7cffdeafaabbc1d5257ac75e2276783ef18fe
sharondocherty <docherty.sharon@gmail.com>
2022-12-22 14:25:12 +0000

    Changed text on query submit page

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -15,7 +15,13 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
 
+       else if (query.toLowerCase().contains("what time is it")) {
+            return "I am not a clock, please look at your watch, don't ask again";
+        }
+
         return "";
     }
+
+
     
 }
